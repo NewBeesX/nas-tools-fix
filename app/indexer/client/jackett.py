4e46a95d5206f159aa350e3fd015121c5eb9d164
qiaoweiyang <qiaoweiyang@bytedance.com>
2023-06-06 23:07:44 +0800

    【FEATURE】增加jackett索引支持

---
+++ app/indexer/client/jackett.py
@@ -0,0 +1,344 @@
+import copy
+import datetime
+import time
+import xml
+
+import requests
+
+import log
+from app.helper import IndexerConf, ProgressHelper, DbHelper
+from app.indexer.client._base import _IIndexClient
+from app.indexer.client._rarbg import Rarbg
+from app.indexer.client._render_spider import RenderSpider
+from app.indexer.client._spider import TorrentSpider
+from app.indexer.client._tnode import TNodeSpider
+from app.indexer.client._torrentleech import TorrentLeech
+from app.sites import Sites
+from app.utils import RequestUtils, ExceptionUtils, StringUtils, DomUtils
+from app.utils.types import SearchType, IndexerType, ProgressKey
+from config import Config
+
+
+class JackettIndexer(_IIndexClient):
+    # 索引器ID
+    client_id = "jackett"
+    # 索引器类型
+    client_type = IndexerType.JACKETT
+    # 索引器名称
+    client_name = IndexerType.JACKETT.value
+
+    # 私有属性
+    _client_config = {}
+    _password = None
+
+    progress = None
+    sites = None
+    dbhelper = None
+
+    def __init__(self, config=None):
+        super().__init__()
+        if config:
+            self._client_config = config
+        else:
+            self._client_config = Config().get_config('jackett')
+        self.init_config()
+
+    def init_config(self):
+        if self._client_config:
+            self.api_key = self._client_config.get('api_key')
+            self._password = self._client_config.get('password')
+            self.host = self._client_config.get('host')
+            if self.host:
+                if not self.host.startswith('http'):
+                    self.host = "http://" + self.host
+                if not self.host.endswith('/'):
+                    self.host = self.host + "/"
+        self.sites = Sites()
+        self.progress = ProgressHelper()
+        self.dbhelper = DbHelper()
+
+    @classmethod
+    def match(cls, ctype):
+        return True if ctype in [cls.client_id, cls.client_type, cls.client_name] else False
+
+    def get_type(self):
+        return self.client_type
+
+    def get_status(self):
+        """
+        检查连通性
+        :return: True、False
+        """
+        if not self.api_key or not self.host:
+            return False
+        return True if self.get_indexers() else False
+
+    def get_indexers(self, check=True, indexer_id=None, public=True):
+        """
+        获取配置的jackett indexer
+        :return: indexer 信息 [(indexerId, indexerName, url)]
+        """
+        # 获取Cookie
+        cookie = None
+        session = requests.session()
+        res = RequestUtils(session=session).post_res(url=f"{self.host}UI/Dashboard",
+                                                     data={"password": self._password})
+        if res and session.cookies:
+            cookie = session.cookies.get_dict()
+        indexer_query_url = f"{self.host}api/v2.0/indexers?configured=true"
+        try:
+            ret = RequestUtils(cookies=cookie).get_res(indexer_query_url)
+            if not ret or not ret.json():
+                return []
+            return [IndexerConf({"id": v["id"],
+                                 "name": v["name"],
+                                 "domain": f'{self.host}api/v2.0/indexers/{v["id"]}/results/torznab/',
+                                 "public": True if v['type'] == 'public' else False,
+                                 "builtin": False})
+                    for v in ret.json()]
+        except Exception as e2:
+            ExceptionUtils.exception_traceback(e2)
+            return []
+
+    def search(self, order_seq,
+               indexer,
+               key_word,
+               filter_args: dict,
+               match_media,
+               in_from: SearchType):
+        """
+        根据关键字多线程搜索
+        """
+        if not indexer or not key_word:
+            return None
+        # 站点流控
+        if self.sites.check_ratelimit(indexer.siteid):
+            self.progress.update(ptype=ProgressKey.Search, text=f"{indexer.name} 触发站点流控，跳过 ...")
+            return []
+        # fix 共用同一个dict时会导致某个站点的更新全局全效
+        if filter_args is None:
+            _filter_args = {}
+        else:
+            _filter_args = copy.deepcopy(filter_args)
+        # 不在设定搜索范围的站点过滤掉
+        if _filter_args.get("site") and indexer.name not in _filter_args.get("site"):
+            return []
+        # 搜索条件没有过滤规则时，使用站点的过滤规则
+        if not _filter_args.get("rule") and indexer.rule:
+            _filter_args.update({"rule": indexer.rule})
+        # 计算耗时
+        start_time = datetime.datetime.now()
+
+        log.info(f"【{self.client_name}】开始搜索Indexer：{indexer.name} ...")
+        # 特殊符号处理
+        search_word = StringUtils.handler_special_chars(text=key_word,
+                                                        replace_word=" ",
+                                                        allow_space=True)
+        # 避免对英文站搜索中文
+        if indexer.language == "en" and StringUtils.is_chinese(search_word):
+            log.warn(f"【{self.client_name}】{indexer.name} 无法使用中文名搜索")
+            return []
+        # 开始索引
+        api_url = f"{indexer.domain}?apikey={self.api_key}&t=search&q={search_word}"
+        result_array = self.__parse_torznabxml(api_url)
+
+        error_flag = len(result_array) == 0
+        # 索引花费的时间
+        seconds = round((datetime.datetime.now() - start_time).seconds, 1)
+        # 索引统计
+        self.dbhelper.insert_indexer_statistics(indexer=indexer.name,
+                                                itype=self.client_id,
+                                                seconds=seconds,
+                                                result='N' if error_flag else 'Y')
+        # 返回结果
+        if error_flag:
+            log.warn(f"【{self.client_name}】{indexer.name} 未搜索到数据")
+            # 更新进度
+            self.progress.update(ptype=ProgressKey.Search, text=f"{indexer.name} 未搜索到数据")
+            return []
+        else:
+            log.warn(f"【{self.client_name}】{indexer.name} 返回数据：{len(result_array)}")
+            # 更新进度
+            self.progress.update(ptype=ProgressKey.Search, text=f"{indexer.name} 返回 {len(result_array)} 条数据")
+            # 过滤
+            return self.filter_search_results(result_array=result_array,
+                                              order_seq=order_seq,
+                                              indexer=indexer,
+                                              filter_args=_filter_args,
+                                              match_media=match_media,
+                                              start_time=start_time)
+
+    def list(self, index_id, page=0, keyword=None):
+        """
+        根据站点ID搜索站点首页资源
+        """
+        if not index_id:
+            return []
+        indexer: IndexerConf = self.get_indexers(indexer_id=index_id)
+        if not indexer:
+            return []
+
+        # 计算耗时
+        start_time = datetime.datetime.now()
+
+        if indexer.parser == "RenderSpider":
+            error_flag, result_array = RenderSpider(indexer).search(keyword=keyword,
+                                                                    page=page)
+        elif indexer.parser == "RarBg":
+            error_flag, result_array = Rarbg(indexer).search(keyword=keyword,
+                                                             page=page)
+        elif indexer.parser == "TNodeSpider":
+            error_flag, result_array = TNodeSpider(indexer).search(keyword=keyword,
+                                                                   page=page)
+        elif indexer.parser == "TorrentLeech":
+            error_flag, result_array = TorrentLeech(indexer).search(keyword=keyword,
+                                                                    page=page)
+        else:
+            error_flag, result_array = self.__spider_search(indexer=indexer,
+                                                            page=page,
+                                                            keyword=keyword)
+        # 索引花费的时间
+        seconds = round((datetime.datetime.now() - start_time).seconds, 1)
+
+        # 索引统计
+        self.dbhelper.insert_indexer_statistics(indexer=indexer.name,
+                                                itype=self.client_id,
+                                                seconds=seconds,
+                                                result='N' if error_flag else 'Y')
+        return result_array
+
+    @staticmethod
+    def __spider_search(indexer, keyword=None, page=None, mtype=None, timeout=30):
+        """
+        根据关键字搜索单个站点
+        :param: indexer: 站点配置
+        :param: keyword: 关键字
+        :param: page: 页码
+        :param: mtype: 媒体类型
+        :param: timeout: 超时时间
+        :return: 是否发生错误, 种子列表
+        """
+        spider = TorrentSpider()
+        spider.setparam(indexer=indexer,
+                        keyword=keyword,
+                        page=page,
+                        mtype=mtype)
+        spider.start()
+        # 循环判断是否获取到数据
+        sleep_count = 0
+        while not spider.is_complete:
+            sleep_count += 1
+            time.sleep(1)
+            if sleep_count > timeout:
+                break
+        # 是否发生错误
+        result_flag = spider.is_error
+        # 种子列表
+        result_array = spider.torrents_info_array.copy()
+        # 重置状态
+        spider.torrents_info_array.clear()
+
+        return result_flag, result_array
+
+    @staticmethod
+    def __parse_torznabxml(url):
+        """
+        从torznab xml中解析种子信息
+        :param url: URL地址
+        :return: 解析出来的种子信息列表
+        """
+        if not url:
+            return []
+        try:
+            ret = RequestUtils(timeout=10).get_res(url)
+        except Exception as e2:
+            ExceptionUtils.exception_traceback(e2)
+            return []
+        if not ret:
+            return []
+        xmls = ret.text
+        if not xmls:
+            return []
+
+        torrents = []
+        try:
+            # 解析XML
+            dom_tree = xml.dom.minidom.parseString(xmls)
+            root_node = dom_tree.documentElement
+            items = root_node.getElementsByTagName("item")
+            for item in items:
+                try:
+                    # indexer id
+                    indexer_id = DomUtils.tag_value(item, "jackettindexer", "id",
+                                                    default=DomUtils.tag_value(item, "prowlarrindexer", "id", ""))
+                    # indexer
+                    indexer = DomUtils.tag_value(item, "jackettindexer",
+                                                 default=DomUtils.tag_value(item, "prowlarrindexer", default=""))
+
+                    # 标题
+                    title = DomUtils.tag_value(item, "title", default="")
+                    if not title:
+                        continue
+                    # 种子链接
+                    enclosure = DomUtils.tag_value(item, "enclosure", "url", default="")
+                    if not enclosure:
+                        continue
+                    # 描述
+                    description = DomUtils.tag_value(item, "description", default="")
+                    # 种子大小
+                    size = DomUtils.tag_value(item, "size", default=0)
+                    # 种子页面
+                    page_url = DomUtils.tag_value(item, "comments", default="")
+
+                    # 做种数
+                    seeders = 0
+                    # 下载数
+                    peers = 0
+                    # 是否免费
+                    freeleech = False
+                    # 下载因子
+                    downloadvolumefactor = 1.0
+                    # 上传因子
+                    uploadvolumefactor = 1.0
+                    # imdbid
+                    imdbid = ""
+
+                    torznab_attrs = item.getElementsByTagName("torznab:attr")
+                    for torznab_attr in torznab_attrs:
+                        name = torznab_attr.getAttribute('name')
+                        value = torznab_attr.getAttribute('value')
+                        if name == "seeders":
+                            seeders = value
+                        if name == "peers":
+                            peers = value
+                        if name == "downloadvolumefactor":
+                            downloadvolumefactor = value
+                            if float(downloadvolumefactor) == 0:
+                                freeleech = True
+                        if name == "uploadvolumefactor":
+                            uploadvolumefactor = value
+                        if name == "imdbid":
+                            imdbid = value
+
+                    tmp_dict = {'indexer_id': indexer_id,
+                                'indexer': indexer,
+                                'title': title,
+                                'enclosure': enclosure,
+                                'description': description,
+                                'size': size,
+                                'seeders': seeders,
+                                'peers': peers,
+                                'freeleech': freeleech,
+                                'downloadvolumefactor': downloadvolumefactor,
+                                'uploadvolumefactor': uploadvolumefactor,
+                                'page_url': page_url,
+                                'imdbid': imdbid}
+                    torrents.append(tmp_dict)
+                except Exception as e:
+                    ExceptionUtils.exception_traceback(e)
+                    continue
+        except Exception as e2:
+            ExceptionUtils.exception_traceback(e2)
+            pass
+
+        return torrents
